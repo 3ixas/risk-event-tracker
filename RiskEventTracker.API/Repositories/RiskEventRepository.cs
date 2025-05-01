2029337ebf323bdad5f3cbe383571002bc77a3ab
Elias Bennett <eliasthebennett@gmail.com>
2025-05-01 13:54:59 +0100

    Refactor repository to use EF Core and SQLite for data persistence.

--- RiskEventTracker.API/Repositories/RiskEventRepository.cs
+++ RiskEventTracker.API/Repositories/RiskEventRepository.cs
@@ -1,44 +1,54 @@
 using RiskEventTracker.API.Models;
+using RiskEventTracker.API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace RiskEventTracker.API.Repositories
 {
     public class RiskEventRepository : IRiskEventRepository
     {
-        private readonly List<RiskEvent> _riskEvents = new List<RiskEvent>();
+        private readonly AppDbContext _context;
+
+        public RiskEventRepository(AppDbContext context)
+        {
+            _context = context;
+        }
 
         public IEnumerable<RiskEvent> GetAll()
         {
-            return _riskEvents;
+            return _context.RiskEvents.AsNoTracking().ToList();
         }
 
         public RiskEvent GetById(Guid id)
         {
-            return _riskEvents.FirstOrDefault(re => re.Id == id);
+            return _context.RiskEvents.Find(id);
         }
 
         public void Add(RiskEvent riskEvent)
         {
-            _riskEvents.Add(riskEvent);
+            _context.RiskEvents.Add(riskEvent);
+            _context.SaveChanges();
         }
 
         public void Update(RiskEvent updatedRiskEvent)
         {
-            var existing = GetById(updatedRiskEvent.Id);
+            var existing = _context.RiskEvents.Find(updatedRiskEvent.Id);
             if (existing != null)
             {
                 existing.Title = updatedRiskEvent.Title;
                 existing.Description = updatedRiskEvent.Description;
                 existing.Type = updatedRiskEvent.Type;
                 existing.Severity = updatedRiskEvent.Severity;
+                _context.SaveChanges();
             }
         }
 
         public void Delete(Guid id)
         {
-            var riskEvent = GetById(id);
+            var riskEvent = _context.RiskEvents.Find(id);
             if (riskEvent != null)
             {
-                _riskEvents.Remove(riskEvent);
+                _context.RiskEvents.Remove(riskEvent);
+                _context.SaveChanges();
             }
         }
     }
